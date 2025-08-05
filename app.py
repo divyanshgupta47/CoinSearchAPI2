37d3f126d12b45d2d4efa4b76260bf6db627ef87
divyanshgupta47 <divyanshgpt47@gmail.com>
2025-08-05 16:48:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from fastapi  import FastAPI, File, UploadFile
 app = FastAPI()
 
 @app.post("/search")
-def search():
+def search(image: UploadFile = File(...)):
 
     return "Search API is working"
 
@@ -13,4 +13,5 @@ def search():
 @app.get("/")
 def read_root():
     return "Hello World!"
+
 
