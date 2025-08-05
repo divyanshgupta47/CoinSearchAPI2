474fc22941932184e358e98777f2c570f37cdba8
divyanshgupta47 <divyanshgpt47@gmail.com>
2025-08-05 17:13:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from fastapi  import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
-
+import tempfile
 
 app = FastAPI()
 
 @app.post("/search")
-def search(image: UploadFile = File(...)):
+async def search(image: UploadFile = File(...)):
     if not image:
         return JSONResponse(content={"error": "No image uploaded"}, status_code=400)
+
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".jpg") as tmp:
+        image_path = tmp.name
+        tmp.write(await image.read())
 
     return "Search API is working"
 
@@ -16,6 +20,7 @@ def search(image: UploadFile = File(...)):
 @app.get("/")
 def read_root():
     return "Hello World!"
+
 
 
 
