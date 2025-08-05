ca06861cbd9d56c191a9e5f8e061ef4bab5446ae
divyanshgupta47 <divyanshgpt47@gmail.com>
2025-08-05 15:51:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,15 @@
-from fastapi  import FastAPI, File, UploadFile
+from fastapi  import FastAPI
 
 import uvicorn
 app = FastAPI()
 
 
-@app.post("/search")
-async def search(image: UploadFile = File(...)):
-    if not image:
-        return JSONResponse(content={"error": "No image uploaded"}, status_code=400)
 
-    return "Search API is working"
 
 @app.get("/")
 def read_root():
     return "Hello World!"
+
 
 
 
