4d3d5a2bb8631d0d8ffad028e6cb8bba8497dd10
divyanshgupta47 <divyanshgpt47@gmail.com>
2025-08-05 16:02:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,19 @@ from fastapi  import FastAPI, File, UploadFile
 
 app = FastAPI()
 
+@app.post("/search")
+async def search(image: UploadFile = File(...)):
+    if not image:
+        return JSONResponse(content={"error": "No image uploaded"}, status_code=400)
+
+    return "Search API is working"
 
 
 
 @app.get("/")
 def read_root():
     return "Hello World!"
+
 
 
 
